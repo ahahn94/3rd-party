96a0a61f5f85d3e950a8d250c820a9090ae01da1
Thomas Staudinger <Staudi.Kaos@gmail.com>
2026-03-15 19:33:26 +0100

    Update RoundPie/PomoDoneApp to 1.5.1594

--- office/pomodoneapp/actions.py
+++ office/pomodoneapp/actions.py
@@ -8,7 +8,7 @@ NoStrip = ["/usr", "/opt"]
 IgnoreAutodep = True
 
 def setup():
-    shelltools.system("ar xf RoundPie-%s_amd64.deb" % get.srcVERSION())
+    shelltools.system("ar xf RoundPie-%s-amd64.deb" % get.srcVERSION())
     shelltools.system("tar xf data.tar.gz")
 
 def install():
